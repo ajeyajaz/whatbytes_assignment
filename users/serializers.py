4b4016e2c68df2ae7c0728fe6e1eceb69db385eb
Ajay H <you@example.com>
2025-06-21 22:30:10 +0530

    add email and password validation

--- users/serializers.py
+++ users/serializers.py
@@ -1,13 +1,26 @@
-from tkinter.font import names
-
+import re
+from django.contrib.auth import  password_validation
 from .models import User
-
 from rest_framework import serializers
+
+# email validation
+def validate_email(email):
+    if not re.match(r"^[\w\.-]+@[\w\.-]+\.\w{2,}$", email):
+        raise  serializers.ValidationError({"error": "Invalid email format"})
+
+    if User.objects.filter(email__iexact=email).exists():
+        raise serializers.ValidationError("Email already exists")
+    return email
+
+# password validation
+def validate_password(password):
+    password_validation.validate_password(password)
+    return password
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=50,allow_blank=False)
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    email = serializers.EmailField(validators=[validate_email])
 
     class Meta:
         model = User
@@ -16,6 +29,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+
 
 
 
