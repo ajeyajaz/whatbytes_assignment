c9d1475f11bae844f8c0f451ccbe055aaaf052cb
Ajay H <you@example.com>
2025-06-22 13:31:03 +0530

    create doctor model

--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Doctor(models.Model):
+    added_by = models.ForeignKey(User,on_delete=models.CASCADE,related_name='doctors')
+    first_name = models.CharField(max_length=100, blank=False)
+    last_name = models.CharField(max_length=100, blank=False)
+    age = models.PositiveIntegerField()
+    specialization = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+
