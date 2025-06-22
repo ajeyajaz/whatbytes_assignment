a9dc45a0d2623b7529dcf8cbd7cf7f6a1daafe79
Ajay H <you@example.com>
2025-06-22 14:32:39 +0530

    create doctor api view

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from .serializers import DoctorSerializer, DoctorListSerializer
+from rest_framework import  permissions, viewsets
+from .pemissions import IsOwnerOrReadonly
+from .models import Doctor
 
-# Create your views here.
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [permissions.IsAuthenticated]
+        else:
+            permission_classes = [IsOwnerOrReadonly]
+        return [permission() for permission in permission_classes]
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return DoctorListSerializer
+        return DoctorSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(added_by=self.request.user)
+
+
+
+
+
+
