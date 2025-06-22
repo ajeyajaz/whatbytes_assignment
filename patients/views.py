e40ddb0aa42129b6bda86b1a26251202004e0290
Ajay H <you@example.com>
2025-06-22 13:16:05 +0530

    create api view'

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets , permissions
+from .serializers import PatientSerializer
+from .models import Patient
 
-# Create your views here.
+class PatientViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = PatientSerializer
+
+    def get_queryset(self):
+        return Patient.objects.filter(added_by=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(added_by=self.request.user)
+
+
+
+
+
+
+
