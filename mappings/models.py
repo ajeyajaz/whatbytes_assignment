0e9cfa4d455f0bc6c325bfbb3a2e5709580230fc
Ajay H <you@example.com>
2025-06-22 15:27:59 +0530

    initial mapping migration

---
+++ mappings/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from doctors.models import Doctor
+from patients.models import Patient
+
+
+class Mapping(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    assigned_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('patient', 'doctor')
+
+
+
